041b13f01b017297bc6ad91b120f43ec38fa4b0d
Bin Yang <ybMaschinenbau@gmail.com>
2023-01-24 21:19:57 +0100

    clean colmap reader

--- PoseEstimation/Apriltag.py
+++ PoseEstimation/Apriltag.py
@@ -2,6 +2,9 @@ from pupil_apriltags import Detector, Detection
 from sys import platform
 from glob import glob
 import cv2
+import os
+from Colmap_Reader import ColmapReader
+
 
 
 if __name__ == '__main__':
@@ -12,53 +15,59 @@ if __name__ == '__main__':
 
     elif platform == "win32":
     # Windows...
-        dataset_path = None
+        dataset_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image100\images_undistorted_prerecorded"
+        database_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image_100_undistorted_prerecorded\Stereo_Fusion.min_num_pixels=10"
 
+
+    VISUALIZATION = False
+
+    database_colmap = ColmapReader(database_path)
+    cameras, images, points3D = database_colmap.read_sparse_model()
+    print(images[1]['name'])
 
     image_list = glob(dataset_path + "/*.jpg")
     image_list.sort()
-    index = 394
-    
-    img = cv2.imread(image_list[index], cv2.IMREAD_GRAYSCALE)
 
     at_detector = Detector(
-    families="tagStandard41h12",
-    nthreads=1,
-    quad_decimate=1.0,
-    quad_sigma=0.0,
-    refine_edges=1,
-    decode_sharpening=0.25,
-    debug=0
-    )
-
-    tags = at_detector.detect(img)
-    print(tags[0].corners)
-
-
-    color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-
-    for tag in tags:
-        for idx in range(len(tag.corners)):
-            cv2.line(
-                color_img,
-                tuple(tag.corners[idx - 1, :].astype(int)),
-                tuple(tag.corners[idx, :].astype(int)),
-                (0, 255, 0),
-            )
-
-        cv2.putText(
-            color_img,
-            str(tag.tag_id),
-            org=(
-                tag.corners[0, 0].astype(int) + 10,
-                tag.corners[0, 1].astype(int) + 10,
-            ),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=0.8,
-            color=(0, 0, 255),
+            families="tagStandard41h12",
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0,
         )
 
-    cv2.imshow("Detected tags", color_img)
+    if VISUALIZATION:
+        index = 80
+        
+        img = cv2.imread(image_list[index], cv2.IMREAD_GRAYSCALE)
 
-    k = cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        tags = at_detector.detect(img)
+        color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+        for tag in tags:
+            for idx in range(len(tag.corners)):
+                cv2.line(
+                    color_img,
+                    tuple(tag.corners[idx - 1, :].astype(int)),
+                    tuple(tag.corners[idx, :].astype(int)),
+                    (0, 255, 0),
+                )
+
+            cv2.putText(
+                color_img,
+                str(tag.tag_id),
+                org=(
+                    tag.corners[0, 0].astype(int) + 10,
+                    tag.corners[0, 1].astype(int) + 10,
+                ),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.8,
+                color=(0, 0, 255),
+            )
+
+        cv2.imshow("Detected tags", color_img)
+
+        k = cv2.waitKey(0)
+        cv2.destroyAllWindows()
--- PoseEstimation/Colmap_Reader.py
+++ PoseEstimation/Colmap_Reader.py
@@ -1,7 +1,5 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-import trimesh
-from trimesh.scene.cameras import Camera
 import math
 from load_ARSceneData import LoadARSceneData
 from sys import platform
@@ -12,27 +10,72 @@ import random
 from glob import glob
 import re
 import os
+from colmap_read_write_model import read_model, detect_model_format
+import open3d as o3d
+
 
 ####### Not in used ######################
 
-'''Read the colmap data
+'''Read the colmap data, give the path to the workspace of colmap reconstruction
 '''
 
 class ColmapReader:
     def __init__(self, path_to_database):
         self.path_to_database = path_to_database
+        self.sparse_model_path = self.get_sparse_model()
+        self.dense_model_path = self.get_dense_model()
+
 
     def get_sparse_model(self):
-        try: 
-            self.sparse_model_path = os.path.join(self.path_to_database , 'sparse')
-        except:
+        sparse_model_path = os.path.join(self.path_to_database , 'sparse')
+        if not os.path.exists(sparse_model_path):
             raise FileNotFoundError('There is no sparse reconstruction in the database, please check if you completely run the colmap code')
-    
+        
+        if detect_model_format(sparse_model_path, ".bin") or detect_model_format(sparse_model_path, ".txt"):
+            return sparse_model_path
+        else:
+            subfolder = os.listdir(sparse_model_path)
+            assert len(subfolder) == 1, "There exists multiple folders in sparse folder, please check it"
+            sparse_model_path = os.path.join(sparse_model_path, subfolder[0])
+
+            if detect_model_format(sparse_model_path, ".bin") or detect_model_format(sparse_model_path, ".txt"):
+                return sparse_model_path
+            else:
+                raise FileNotFoundError('There is no sparse reconstruction in the database, please check if you completely run the colmap code')
+
+    # ToDo: detect the .ply file
     def get_dense_model(self):
-        try:
-            self.dense_model_path = os.path.join(self.path_to_database , 'dense')
-        except:
+        
+        dense_model_path = os.path.join(self.path_to_database , 'dense')
+        if not os.path.exists(dense_model_path):
             raise FileNotFoundError('There is no dense reconstruction in the database, please check if you completely run the colmap code')
+
+        if os.path.isfile(os.path.join(dense_model_path, "fused.ply")):
+            pass
+        else:
+            subfolder = os.listdir(dense_model_path)
+            assert len(subfolder) == 1, "There exists multiple folders in sparse folder, please check it"
+
+            dense_model_path = os.path.join(dense_model_path, subfolder[0])
+
+        return dense_model_path
+    
+    def read_sparse_model(self):
+        cameras, images, points3D = read_model(self.sparse_model_path)
+
+        return cameras, images, points3D
+
+    def read_dense_model(self):
+        pcd = o3d.io.read_point_cloud(os.path.join(self.dense_model_path, "fused.ply"))
+
+        if os.path.isfile(os.path.join(self.dense_model_path, "meshed-poisson.ply")):
+            mesh = o3d.io.read_triangle_mesh(os.path.join(self.dense_model_path, "meshed-poisson.ply"))
+        else:
+            print("There is no possion mesh")
+            mesh = None
+
+        return pcd, mesh
+
 
     @staticmethod
     def rewrite_image_txt(path_to_file):
@@ -68,3 +111,22 @@ class ColmapReader:
         camera_params = np.array(camera_params)
 
         return image_id, camera_params, points_2D, point3D_IDs
+
+
+if __name__ == '__main__':
+    if platform == "linux" or platform == "linux2":  
+    # linux
+        dataset_path = r""
+
+    elif platform == "win32":
+    # Windows...
+        dataset_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image_100_undistorted_prerecorded\Stereo_Fusion.min_num_pixels=10"
+
+    data = ColmapReader(dataset_path)
+    cameras, images, points3D = data.read_sparse_model()
+    # pcd_dense, mesh_dense = data.read_dense_model()
+    # o3d.visualization.draw_geometries([pcd_dense])
+
+    print(cameras.items()[0])
+    #print(images.items())
+    #print(points3D.items())
