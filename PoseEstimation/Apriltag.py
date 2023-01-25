16954fa5b57069e831d4995bf181bc4870c4a7bc
Bin Yang <ybMaschinenbau@gmail.com>
2023-01-25 11:32:44 +0100

    apriltag

--- PoseEstimation/Apriltag.py
+++ PoseEstimation/Apriltag.py
@@ -4,49 +4,15 @@ from glob import glob
 import cv2
 import os
 from Colmap_Reader import ColmapReader
+import numpy as np
+import collections
 
+BaseMask = collections.namedtuple(
+    "Mask", ["tag_id", "tag_corners", "tag_center", "pixels_inside_tag"])
 
-
-if __name__ == '__main__':
-
-    if platform == "linux" or platform == "linux2":  
-    # linux
-        dataset_path = r"/home/biyang/Documents/3D_Gaze/dataset/PupilInvisible/room1/images"
-
-    elif platform == "win32":
-    # Windows...
-        dataset_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image100\images_undistorted_prerecorded"
-        database_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image_100_undistorted_prerecorded\Stereo_Fusion.min_num_pixels=10"
-
-
-    VISUALIZATION = False
-
-    database_colmap = ColmapReader(database_path)
-    cameras, images, points3D = database_colmap.read_sparse_model()
-    print(images[1]['name'])
-
-    image_list = glob(dataset_path + "/*.jpg")
-    image_list.sort()
-
-    at_detector = Detector(
-            families="tagStandard41h12",
-            nthreads=1,
-            quad_decimate=1.0,
-            quad_sigma=0.0,
-            refine_edges=1,
-            decode_sharpening=0.25,
-            debug=0,
-        )
-
-    if VISUALIZATION:
-        index = 80
-        
-        img = cv2.imread(image_list[index], cv2.IMREAD_GRAYSCALE)
-
-        tags = at_detector.detect(img)
-        color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-
-        for tag in tags:
+def visualize(img_grayscale, tags):
+    color_img = cv2.cvtColor(img_grayscale, cv2.COLOR_GRAY2RGB)
+    for tag in tags:
             for idx in range(len(tag.corners)):
                 cv2.line(
                     color_img,
@@ -67,7 +33,83 @@ if __name__ == '__main__':
                 color=(0, 0, 255),
             )
 
-        cv2.imshow("Detected tags", color_img)
+    cv2.imshow("Detected tags", color_img)
 
-        k = cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    k = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def get_mask(img_grayscale, tags, visualization = True):
+    print("There are totally {} tags in the frame".format(len(tags)))
+    masks = []
+    for tag in tags:
+        mask = np.zeros((img.shape), dtype=np.uint8)
+        pts = np.array(tag.corners.astype(int))
+        cv2.fillPoly(mask, [pts], (255))
+
+    return
+        
+
+if __name__ == '__main__':
+
+    if platform == "linux" or platform == "linux2":  
+    # linux
+        dataset_path = r"/home/biyang/Documents/3D_Gaze/Colmap/PI_room1/Test_image100_undistorted_chessboard/images_undistorted_chessboard"
+        database_path = r"/home/biyang/Documents/3D_Gaze/Colmap/PI_room1/Test_image100_undistorted_chessboard/"
+
+    elif platform == "win32":
+    # Windows...
+        dataset_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image100\images_undistorted_prerecorded"
+        database_path = r"D:\Documents\Semester_Project\3D_Gaze\dataset\PupilInvisible\room1\image_100_undistorted_prerecorded\Stereo_Fusion.min_num_pixels=10"
+
+
+    VISUALIZATION = False
+
+    database_colmap = ColmapReader(database_path)
+    cameras, images, points3D = database_colmap.read_sparse_model()
+
+    # Test
+    test_frame = images[100]
+    print(np.array(test_frame.xys).shape)
+    print(np.where(test_frame.point3D_ids != -1)[0].shape)
+    print(test_frame.point3D_ids.shape)
+
+    at_detector = Detector(
+            families="tagStandard41h12",
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0,
+        )
+
+    img_fullpath = os.path.join(dataset_path, test_frame.name)
+    img = cv2.imread(img_fullpath, cv2.IMREAD_GRAYSCALE)
+    tags = at_detector.detect(img)
+
+    # print(tags[0].corners)
+    # print(tags[0].center)
+    mask = np.zeros((img.shape), dtype=np.uint8)
+    pts = np.array(tags[0].corners.astype(int))
+    print(pts)
+    cv2.fillPoly(mask, [pts], (255))
+
+    pixels = np.where(mask == 255)
+    print(pixels)
+    # cv2.imshow('mask', mask)
+    # k = cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    if VISUALIZATION:
+        image_list = glob(dataset_path + "/*.jpg")
+        image_list.sort()
+        index = 80
+        
+        img = cv2.imread(image_list[index], cv2.IMREAD_GRAYSCALE)
+
+        tags = at_detector.detect(img)
+
+        visualize(img, tags)
+        
+
+        
